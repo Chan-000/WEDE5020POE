f89d1eea3ce86f3335f4480cc8754b26ffbd59db
Chantel Tafadzwa Chiraswa <ST10497253@rrconnect.edu.za>
2025-11-14 12:47:27 +0200

    Fixed: live search now - filters products by name

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
-<script>
-  console.log("JS is working");
-</script>
+// script.js working search filter
+console.log("script.js loaded");
+
+// wait for the page to fully load
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("page loaded. Ready to search.");
+
+  //check if input exists
+  const searchInput = document.getElementById("searchInput");
+  if (!searchInput) {
+    console.error("ERROR: #searchInput not found");
+    return;
+  }
+
+  //check if products exists
+  const products = document.querySelectorAll(".product-item");
+  if (products.length == 0) {
+    console.error("Error: no .product-item found!");
+    return;
+  }
+
+  //search function
+  window.filterProducts = function () {
+    const input = searchInput.value.toLowerCase();
+    console.log("searching for: ", input);
+
+    products.forEach(product => {
+      const text = product.textContent.toLowerCase();
+      if (text.includes(input)) {
+        product.style.display = "block";
+      } else {
+        product.style.display = "none";
+      }
+    });
+  };
+  filterProducts();
+
+});
